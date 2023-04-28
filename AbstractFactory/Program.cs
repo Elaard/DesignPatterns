f4df9d686a730a692680c6fb69ce595083c97299
Elaard <bartek.dv.89@gmail.com>
2023-04-28 17:32:19 +0200

    feat: add basic design patterns

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContinentFactory africa = new AfricaFactory();
+            AnimalWorld world = new AnimalWorld(africa);
+
+            world.RunFoodChain();
+
+            Console.ReadKey();
+        }
+    }
+    // Use the Abstract Factory when your code needs to work with various families of related products,
+    // but you don’t want it to depend on the concrete classes of those products—they might be unknown beforehand
+    // or you simply want to allow for future extensibility
+
+    abstract class ContinentFactory
+    {
+        public abstract Herbivore CreateHerbivore();
+        public abstract Carnivore CreateCarnivore();
+    }
+
+    class AfricaFactory : ContinentFactory
+    {
+        public override Carnivore CreateCarnivore()
+        {
+            return new Lion();
+           
+        }
+
+        public override Herbivore CreateHerbivore()
+        {
+            return new Wildebeest();
+        }
+    }
+
+    class AmericaFactory : ContinentFactory
+    {
+        public override Herbivore CreateHerbivore()
+        {
+            return new Bison();
+        }
+
+        public override Carnivore CreateCarnivore()
+        {
+            return new Wolf();
+        }
+    }
+
+    abstract class Herbivore
+    {
+
+    }
+
+    abstract class Carnivore
+    {
+        public abstract void Eat(Herbivore h);
+    }
+
+    class Wildebeest : Herbivore
+    {
+    }
+
+    class Bison : Herbivore
+    {
+    }
+
+    class Lion : Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+            Console.WriteLine(this.GetType().Name +
+              " eats " + h.GetType().Name);
+        }
+    }
+
+    class Wolf : Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+            Console.WriteLine(this.GetType().Name +
+              " eats " + h.GetType().Name);
+        }
+    }
+
+    //Client class
+    class AnimalWorld
+    {
+        private Herbivore _herbivore;
+        private Carnivore _carnivore;
+        // Constructor
+        public AnimalWorld(ContinentFactory factory)
+        {
+            _carnivore = factory.CreateCarnivore();
+            _herbivore = factory.CreateHerbivore();
+        }
+        public void RunFoodChain()
+        {
+            _carnivore.Eat(_herbivore);
+        }
+    }
+
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FactoryMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Document[] documents = new Document[3];
+
+            documents[0] = new Resume();
+            documents[1] = new Report();
+            // Display document pages
+            foreach (Document document in documents)
+            {
+                Console.WriteLine("\n" + document.GetType().Name + "--");
+                foreach (Page page in document.Pages)
+                {
+                    Console.WriteLine(" " + page.GetType().Name);
+                }
+            }
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Page { }
+
+    class SkillsPage: Page { }
+
+    class EducationPage: Page { }
+
+    class ExperiencePage: Page { }
+
+    class IntroductionPage: Page { }
+
+    class ResultsPage: Page { }
+
+    class ConclusionPage : Page {}
+
+    class BibliographyPage : Page { }
+
+    class SummaryPage : Page { }
+
+    abstract class Document
+    {
+        private List<Page> _pages = new List<Page>();
+
+        public Document()
+        {
+            this.CreatePages();
+        }
+
+        public List<Page> Pages {
+            get { return _pages;  }
+        }
+
+        public abstract void CreatePages();
+    }
+
+    class Resume: Document
+    {
+        public override void CreatePages()
+        {
+            Pages.Add(new SkillsPage());
+            Pages.Add(new EducationPage());
+            Pages.Add(new ExperiencePage());
+        }
+    }
+
+    class Report : Document
+    {
+        // Factory Method implementation
+        public override void CreatePages()
+        {
+            Pages.Add(new IntroductionPage());
+            Pages.Add(new ResultsPage());
+            Pages.Add(new ConclusionPage());
+            Pages.Add(new SummaryPage());
+            Pages.Add(new BibliographyPage());
+        }
+    }
+
+}
