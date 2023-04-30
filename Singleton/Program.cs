db3d6506d15c40a88ba3be7a0b4444126a72d904
Elaard <bartek.dv.89@gmail.com>
2023-04-30 14:33:02 +0200

    feat: add singleton example

---
+++ Singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    // defines an Instance operation that lets clients access its unique instance. Instance is a class operation
+    // can be use as database connection if u want to have only one
+    class Singleton
+    {
+        static Singleton instance;
+        private Singleton()
+        {
+            
+        }
+        
+        public static Singleton GetInstance ()
+        {
+            if(instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+    }
+}
